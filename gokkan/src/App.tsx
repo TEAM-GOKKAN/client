8221700594225dc54d668f7c974e9beaa3af7f83
lianKim <5ffcut@gmail.com>
2022-12-26 18:38:41 +0900

    Design: global style 설정

--- gokkan/src/App.tsx
+++ gokkan/src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import GlobalStyle from './lib/styles/global';
 
 const LayoutPage = lazy(() => import('./pages/LayoutPage'));
 const MainPage = lazy(() => import('./pages/MainPage'));
@@ -7,6 +8,7 @@ const MainPage = lazy(() => import('./pages/MainPage'));
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<LayoutPage />}>
           <Route path="/" element={<MainPage />} />
